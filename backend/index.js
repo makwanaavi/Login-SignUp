df2be4fec67d11ebb70fb522a230fabfe3cf0c31
prabhu-woyce <pparmar@woyce.io>
2025-07-21 10:47:02 +0530

    #update

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/home", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.get('/about', (req, res)=>{
+    res.send("About Page")
+})
